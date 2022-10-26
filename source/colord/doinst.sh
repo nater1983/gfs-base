95df751c463e9634761616188bc36adb84d67072
Nathaniel Russell <46272571+nater1983@users.noreply.github.com>
2022-10-26 18:13:44 -0500

    Update doinst.sh

--- source/colord/doinst.sh
+++ source/colord/doinst.sh
@@ -15,3 +15,8 @@ if [ -e usr/share/glib-2.0/schemas ]; then
     /usr/bin/glib-compile-schemas usr/share/glib-2.0/schemas >/dev/null 2>&1
   fi
 fi
+
+getent group colord >/dev/null || groupadd -r colord
+getent passwd colord >/dev/null || \
+    useradd -r -g colord -d /var/lib/colord -s /sbin/nologin \
+    -c "User for colord" colord
